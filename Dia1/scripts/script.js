36e650d174050babdb0fa96fd847cc05242e6e83
NicoleArias <nicoledayanaariaspinzon@gmail.com>
2025-07-18 12:25:31 -0500

    feat :sparkles: Intro JavaScript

---
+++ Dia1/scripts/script.js
@@ -0,0 +1,41 @@
+// ### DIA 1 ### //
+
+console.log ("Hola Mundo!");
+variable1=3.14;
+console.log(variable1)
+console.log(typeof (variable1))
+
+//Concatenación//
+palabra1 = "Campus"
+palabra2 = "Lands"
+palabra3 = palabra1+palabra2;
+console.log(palabra3);
+
+nombrePersona = "Luis Angel";
+palabraActual = 55000;
+console.log ("La persona llamada" +nombrePersona+ "tiene en su cuenta bancaria" +plataActual+ "pesos colombianos!");
+
+//Booleanos
+booleanito1 = true;
+booleanito2 = false;
+console.log(typeof (booleanito1));
+
+//Conditionals
+if (booleanito == True){
+    console.log("Es verdadero")
+}else{
+    console.log("No es verdadero :sadfeis:");
+}
+
+//Loops
+for (i=0 ; i<4; i++ ){
+    console.log("Wenas")
+}
+
+hola = true;
+while (hola == true){
+    console.log("Wenass")
+    hola = false
+}
+
+// Desarrolado por : Nicole Arias - c.c; 1,097,496,027
