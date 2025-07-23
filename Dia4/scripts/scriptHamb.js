7467b69b732fdca9f87d4ea6a31fbf6733655fb4
NicoleArias <nicoledayanaariaspinzon@gmail.com>
2025-07-23 11:22:41 -0500

    feat :pushpin: menu added

---
+++ Dia4/scripts/scriptHamb.js
@@ -0,0 +1,18 @@
+// Quiz relámpago
+
+let hamb = []
+
+pedido = true
+while ( pedido == true){
+    let opcion = prompt (`
+    Bienvenido! Escribe la opcion que deseas realizar
+    1. Hacer un pedido
+    2. Ordenar desde el Menu
+    3. Personalizar un producto
+    4. Eliminar
+    `);
+
+    if (opcion === 1){
+        let 
+    };
+}
