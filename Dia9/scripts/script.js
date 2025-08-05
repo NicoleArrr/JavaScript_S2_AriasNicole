f09bf982ae71bebf59c3660d02f15c4a46bc9e1d
NicoleArias <nicoledayanaariaspinzon@gmail.com>
2025-08-04 20:01:08 -0500

    feat :sparkles: PokéApi interactive part added

--- Dia9/scripts/script.js
+++ Dia9/scripts/script.js
@@ -1,37 +1,35 @@
 // PokéAPI
+function mostrarInfo(event) {
+    if (event.key === 'Enter') {
+        buscarPokemon()
+    }
+}
 
 function buscarPokemon() {
-    document.getElementById("pantalla").innerHTML=``;
-    const busqPoke = document.getElementById("nombreInput").value.trim();
+    document.getElementById("personaje").innerHTML = ``;
+    const busqPoke = document.getElementById("buscarPersonaje").value.trim().toLowerCase();
     console.log(busqPoke);
-    const xhr = new XMLHttpRequest();//La importación del XML
+    const xhr = new XMLHttpRequest();
     const url = `https://pokeapi.co/api/v2/pokemon/${busqPoke}`;
     console.log(url);
     xhr.open("GET", url, true);
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 3) {
-
             console.log("Cargando...");
         }
         else if (xhr.readyState === 4 && xhr.status === 200) {
             try {
                 const datos = JSON.parse(xhr.responseText);
-                if (datos.results && datos.results.length > 0) {
-                    for (let i = 0; i < datos.results.length; i++) {
-                        let division = document.getElementById("pantalla");
-                        division.innerHTML += `
-                            <img src="${datos["sprites"][i]["showdown"]["front_default"]}" alt="">
-                            <h3>${datos["results"][i]["name"]}</h3>
-                            <p><strong>Status:</strong>${datos["results"][i]["status"]}</p>
-                            <p><strong>Specie:</strong>${datos["results"][i]["species"]}</p>
-                        `
-                        console.log(datos[i]["id"]);
-                    }
-                }
-
+                let busqueda = document.getElementById("personaje");
+                busqueda.innerHTML += `
+                    <img src="${datos["sprites"]["other"]["showdown"]["front_default"]}" alt="">
+                    <h2>${datos["id"]} - ${datos["name"]}</h2>
+                    `
+                console.log(datos["id"])
             }
             catch (err) {
+                alert ('Error en la poke búsqueda')
                 console.log(err.message);
             }
         }
