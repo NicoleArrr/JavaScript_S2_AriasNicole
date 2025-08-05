dba1b46272f0d71f4efdd0710ecda3ae49926dc2
NicoleArias <nicoledayanaariaspinzon@gmail.com>
2025-08-05 13:26:49 -0500

    feat :sparkles: work in class

---
+++ Dia13/scripts/script.js
@@ -0,0 +1,39 @@
+function mostrarInfo(event) {
+    if (event.key === 'Enter') {
+        buscarSuperhero()
+    }
+}
+
+function buscarSuperhero() {
+    const personaje = document.getElementById("personaje")
+    personaje.innerHTML = ''
+    const buscarSuperhero = document.getElementById("buscarPersonaje").value.trim().toLowerCase();
+    console.log(buscarSuperhero);
+    const xhr = new XMLHttpRequest();
+    const url = `https://www.superheroapi.com/api.php/7e1457414f866c52535e1184cf92b42c/search/${buscarSuperhero}`;
+    console.log(url);
+    xhr.open("GET", url, true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 3) {
+            console.log("Cargando...");
+        }
+        else if (xhr.readyState === 4 && xhr.status === 200) {
+            try {
+                const datos = JSON.parse(xhr.responseText);
+                console.log(datos);
+                personaje.innerHTML = `
+                <h2>${datos["results"][0]["name"]}</h2>
+                <p>${datos["results"][0]["biography"]["full-name"]}</p>
+                <p>${datos["results"][0]['appearance']['gender']}</p>
+                `
+                console.log(datos["results"][0]);
+            }
+            catch (err) {
+                alert('Error en la búsqueda')
+                console.error(err.message);
+            }
+        }
+    };
+    xhr.send();
+}
